93c339d4f43a52cf755abbbe5aea1c2cb90f8fb7
sitimeirizkimelanii <siti_melani_23rpl@student.smktelkom-mlg.sch.id>
2016-10-27 10:03:30 +0700

    Initial Commit

---
+++ app/src/androidTest/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/ApplicationTest.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl304142434.belajariqro;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
