b7231e867d23f43c3689b1d49c61d5ec9ef003dd
nurlinda <nurlinda.rhm2812@gmail.com>
2016-11-16 14:13:44 +0700

    Tampilan awal dan menu awal

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/menu_awal.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/menu_awal.java
@@ -1,15 +1,13 @@
 package id.sch.smktelkom_mlg.project.xiirpl304142434.belajariqro;
 
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class menu_awal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_menu_awal);
     }
 }
