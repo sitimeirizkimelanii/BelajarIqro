eb724643a44c9c83acba5e04ddaf1db79581d02b
nurlinda <nurlinda.rhm2812@gmail.com>
2016-11-19 15:22:39 +0700

    tambah_activity

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/bljr_iqro.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl304142434.belajariqro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bljr_iqro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bljr_iqro);
+    }
+}
