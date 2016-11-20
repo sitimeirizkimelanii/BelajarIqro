b88f3298d43c51bbc51c4796bfdbadb4e51e49e2
sitimeirizkimelanii <siti_melani_23rpl@student.smktelkom-mlg.sch.id>
2016-11-21 00:26:11 +0700

    Layout Quiz 1,2,3,4

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/Quiz3Activity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl304142434.belajariqro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Quiz3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz3);
+    }
+}
