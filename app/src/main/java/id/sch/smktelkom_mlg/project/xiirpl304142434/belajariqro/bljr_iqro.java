040fadbe196bfdfaa2a5bade2f5f833971041231
erine raharjo <erineraharjo17@gmail.com>
2016-11-20 22:43:53 +0700

    commithijaiyah1

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/bljr_iqro.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/bljr_iqro.java
@@ -7,12 +7,13 @@ import android.view.View;
 
 public class bljr_iqro extends AppCompatActivity {
     private static final String TAG = "lagu";
-    MediaPlayer play;
+    MediaPlayer play, play2, play3, play4, play5, play6, play7, play8, play9, play10, play11, play12, play13, play14, play15,
+    play16, play17, play18, play19, play20, play21, play22, play23, play24, play25, play26, play27, play28, play29, play30;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bljr_iqro);
-        play = MediaPlayer.create(this, R.raw.song);
+        play = MediaPlayer.create(this, R.raw.alef);
         findViewById(R.id.button_a).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,6 +22,129 @@ public class bljr_iqro extends AppCompatActivity {
 
             private void doclick() {
                 play.start();
+            }
+        });
+
+        play2 = MediaPlayer.create(this, R.raw.ba);
+        findViewById(R.id.imageButton3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() {
+                play2.start();
+            }
+        });
+
+        play3 = MediaPlayer.create(this, R.raw.ta);
+        findViewById(R.id.imageButton4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() {
+                play3.start();
+            }
+        });
+
+        play5 = MediaPlayer.create(this, R.raw.jim);
+        findViewById(R.id.imageButton6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play5.start();
+            }
+        });
+
+        play8 = MediaPlayer.create(this, R.raw.dal);
+        findViewById(R.id.imageButton9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play8.start();
+            }
+        });
+
+        play10 = MediaPlayer.create(this, R.raw.ra);
+        findViewById(R.id.imageButton11).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play10.start();
+            }
+        });
+
+        play22 = MediaPlayer.create(this, R.raw.kaf);
+        findViewById(R.id.imageButton23).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play22.start();
+            }
+        });
+
+        play23 = MediaPlayer.create(this, R.raw.lam);
+        findViewById(R.id.imageButton24).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play23.start();
+            }
+        });
+
+        play24 = MediaPlayer.create(this, R.raw.meem);
+        findViewById(R.id.imageButton25).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play24.start();
+            }
+        });
+
+        play25 = MediaPlayer.create(this, R.raw.noon);
+        findViewById(R.id.imageButton26).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play25.start();
+            }
+        });
+
+        play26 = MediaPlayer.create(this, R.raw.waw);
+        findViewById(R.id.imageButton27).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play26.start();
+            }
+        });
+
+        play27 = MediaPlayer.create(this, R.raw.hamza);
+        findViewById(R.id.imageButton28).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() { play27.start();
             }
         });
     }
