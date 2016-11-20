47b9a3929f7fd66c8a480ff40effd08803686877
nurlinda <nurlinda.rhm2812@gmail.com>
2016-11-20 14:57:14 +0700

    splash screen, button huruf hijjaiyah dan button menu awal

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/bljr_iqro.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/bljr_iqro.java
@@ -1,13 +1,29 @@
 package id.sch.smktelkom_mlg.project.xiirpl304142434.belajariqro;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class bljr_iqro extends AppCompatActivity {
-
+    private static final String TAG = "lagu";
+    MediaPlayer play;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bljr_iqro);
+        play = MediaPlayer.create(this, R.raw.song);
+        findViewById(R.id.button_a).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doclick();
+            }
+
+            private void doclick() {
+                play.start();
+            }
+        });
     }
+
+
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/menu_awal.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl304142434/belajariqro/menu_awal.java
@@ -1,7 +1,10 @@
 package id.sch.smktelkom_mlg.project.xiirpl304142434.belajariqro;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class menu_awal extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class menu_awal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_awal);
+
+        ImageButton belajariqro = (ImageButton) findViewById(R.id.belajar_iqro);
+        belajariqro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), bljr_iqro.class);
+                startActivity(i);
+            }
+        });
     }
 }
